d31f9d28e033e3b4eae5e763e1b42cb20df3a021
hiroakilucas <hiroaki.lucas@gmail.com>
2014-08-20 17:53:32 -0300

    aprendendi git e github

--- projetoGit/src/projetoGit/Ieheeh.java
+++ projetoGit/src/projetoGit/Ieheeh.java
@@ -4,11 +4,10 @@ public class Ieheeh {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("Funfou!!!!!3");
-=======
-		System.out.println("Funfou!!!!!2");
->>>>>>> 90d39cd5bd3660f69363b8fafc194817fe9be6b2
+
+		System.out.println("Aprendendo a usar Git e GitHub!!!!!");
+
+
 	}
 
 }
