eafa8018a217cfc692f50f45ea61352428edff81
Willian Joddy Kaneko <willian.kaneko@resource.com.br>
2014-08-21 17:20:58 -0300

    teste

--- projetoGit/src/projetoGit/GitHub.java
+++ projetoGit/src/projetoGit/GitHub.java
@@ -6,6 +6,7 @@ public class GitHub {
 		// TODO Auto-generated method stub
 
 		System.out.println("Aprendendo a usar Git e GitHub!!!!!");
+		System.out.println("Aprendendo a usar Git e GitHub!!!!!");
 
 
 	}
