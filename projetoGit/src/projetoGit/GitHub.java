6b829ffd13dfd9d44978daa8385ee795c44c0812
hiroakilucas <hiroaki.lucas@gmail.com>
2014-08-22 11:15:30 -0300

    merge

--- projetoGit/src/projetoGit/GitHub.java
+++ projetoGit/src/projetoGit/GitHub.java
@@ -7,6 +7,7 @@ public class GitHub {
 
 		System.out.println("Aprendendo a usar Git e GitHub!!!!!");
 		System.out.println("Aprendendo a usar Git e GitHub!!!!!");
+		System.out.println("Aprendendo a usar Git e GitHub merge!!!!!");
 
 
 	}
