8b959e08eb61cd9d5fee39991197b0be4bb58a33
hiroakilucas <hiroaki.lucas@gmail.com>
2014-08-22 14:23:52 -0300

    teste

--- projetoGit/src/projetoGit/GitHub.java
+++ projetoGit/src/projetoGit/GitHub.java
@@ -4,27 +4,7 @@ package projetoGit;
 public class GitHub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Aprendendo a usar Git e GitHub merge!!!!!");
-		System.out.println("Aprendendo a usar Git e GitHub!!!!!");
-		System.out.println("Aprendendo a usar Git e GitHub!!!!!");
-		System.out.println("Aprendendo a usar Git e GitHub merge!!!!!");
-		System.out.println("Aprendendo a usar Git e GitHub merge!!!!!");
-		System.out.println("Aprendendo a usar tsteMergeGit e GitHub merge!!!!!");
-		System.out.println("Aprendendo a usar Git e GitHub merge!!!!!");
-		//Merge1
-		//huehuehue
-		System.out.println("Aprendendo a usar Git e GitHub merge!!!!!");
-		//merge1
-		System.out.println("MERGE");
-		//Merge2
-		//huehuehue
-		//merge2
-		System.out.println("MERGE");
-		//merge3
-		System.out.println("MERGE");
-		//Merge3
-		//huehuehue
+		//mergefetch
 		
 	}
 
